e68542b219de37efacf8aaf34b21fbd5467ebb6c
Raghavendra Koduru <119060193+Raghavendrakoduru@users.noreply.github.com>
2024-06-06 11:06:56 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //variable && constant
+      int a,b,c;
+      a= 5;
+      b=2;
+      c=a+b;
+      cout<<c<<endl;
+      cout<<a+b<<endl<<"a+b";
+      return 0;
+}
